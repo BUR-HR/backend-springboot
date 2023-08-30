4807d8cbbfe28ce0cb8a3fb7d9736cf79a3004fd
yuju <zzgmlo3o@naver.com>
2023-08-30 22:16:37 +0900

    file

--- src/main/java/com/bubblebubble/hr/login/dto/EmployeeDTO.java
+++ src/main/java/com/bubblebubble/hr/login/dto/EmployeeDTO.java
@@ -1,87 +1,106 @@
-package com.bubblebubble.hr.login.dto;
+    package com.bubblebubble.hr.login.dto;
 
-import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+    import lombok.*;
+    import org.springframework.security.core.GrantedAuthority;
+    import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.Date;
+    import java.util.Collection;
+    import java.util.Date;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
-public class EmployeeDTO implements UserDetails {
-    private int empNo;
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    @ToString
+    public class EmployeeDTO implements UserDetails {
+        private int empNo;
 
-    private String empName;
+        private String empName;
 
-    private String deptCode;
+        private String deptCode;
 
-    private String jobCode;
+        private String jobCode;
 
-    private String employeeRsdn;
+        private String employeeRsdn;
 
-    private String employeeEmail;
+        private String employeeEmail;
 
-    private String employeePassword;
+        private String employeePassword;
 
-    private Date hireDate;
+        private Date hireDate;
 
-    private String employeePhone;
+        private String employeePhone;
 
-    private String employeeAddress;
+        private String employeeAddress;
 
-    private String payrollAcoount;
+        private String payrollAcoount;
 
-    private Character isEmployed;
+        private Character isEmployed;
 
-    private String employeeStatus;
+        private String employeeStatus;
 
-    private String employeeGender;
+        private String employeeGender;
 
-    private Date leaveDate;
-    private Collection<? extends GrantedAuthority> authorities;
+        private Date leaveDate;
 
-    @Override
-    public String getUsername() {
-        return this.employeeEmail; // Assuming email is used as the username
+        private Collection<? extends GrantedAuthority> authorities;
+
+        private String originFile;
+
+        private String renameFile;
+
+        @Override
+        public String getUsername() {
+            return this.employeeEmail;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            // Add your implementation here
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            // Add your implementation here
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            // Add your implementation here
+            return true;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return this.authorities;
+        }
+
+        @Override
+        public String getPassword() {
+            return this.employeePassword;
+        }
+
+        public String getOriginFile() {
+            return originFile;
+        }
+
+        public void setOriginFile(String originFile) {
+            this.originFile = originFile;
+        }
+
+        public String getRenameFile() {
+            return renameFile;
+        }
+
+        public void setRenameFile(String renameFile) {
+            this.renameFile = renameFile;
+        }
     }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        // Add your implementation here
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        // Add your implementation here
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        // Add your implementation here
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        // Add your implementation here
-        return true;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.authorities;
-    }
-
-    @Override
-    public String getPassword() {
-        return this.employeePassword;
-    }
-
-}
 
